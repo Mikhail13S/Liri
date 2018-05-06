a78cc2321189f4607eb2e9ad09356bc1d90851a2
Frey39 <35135008+Mikhail13S@users.noreply.github.com>
2018-05-05 22:59:02 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,10 @@
+console.log('The bot is working');
+
+var Twit = require('twit');
+
+var T = new Twit({
+    consumer_key:         'sU1gIQ1QwPwsSiYbbxx6Avg3e',
+    consumer_secret:      'My5qVCzBpdHkfbjesnM1YWlGQfv1HliTY4CfgtgRRV05T6voSF',
+    access_token:         '989662838594646016-bg809Or01DzyGSTecu6esRMIVrHbgFU',
+    access_token_secret:  'qeYltlbUGcvQgQQQMA6BdwLmdpwLh63VdtuwUm9sRjsQt',
+})
